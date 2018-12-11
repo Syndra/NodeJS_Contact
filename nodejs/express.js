72ab3ed8969dfa0a6e6d768d5bdb7b7ff70030b7
lenny-kong <forlennykong@gmail.com>
2018-12-12 01:12:26 +0900

    dff

--- nodejs/express.js
+++ nodejs/express.js
@@ -11,43 +11,20 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'html')));
 
-// app.get('/', (req, res) => {
-//   console.log("IN");
-//   fs.readFile('html/index.html', 'utf-8', function(error, datas){
-//     if(error)
-//       console.log('Readfile error');
-//     else{
-//       res.send(ejs.render(datas, {contactsList:NULL}));
-//
-//     }
-//   });
-// });
-
 app.get('/all', (req, res) => {
-  //res.sendFile(path.join(__dirname, 'html', 'index.html'));
-  default_op(req, res);
+  default_op_logs(req, res);
 });
 
-app.post('/list', (req, res) => {
-  //res.sendFile(path.join(__dirname, 'html', 'index.html'));
-  default_op(req, res);
+app.post('/searchlogs', (req, res) => {
+  search_op_logs(req, res);
 });
 
-app.post('/insert', (request, response) => {
-  insert_op(request, response);
-  //response.sendFile(path.join(__dirname, 'html', 'list.html'));
+app.post('/searchwakeuplog', (req, res) => {
+  search_op_wakeuplog(req, res);
 });
 
-app.post('/search', (request, response) => {
-  search_op(request, response);
-});
-
-app.post('/delete', (request, response) => {
-  delete_op(request, response);
-});
-
-app.post('/update', (request, response) => {
-  update_op(request, response);
+app.post('/searchavg', (req, res) => {
+  search_op_avg(req, res);
 });
 
 app.listen(3000, () => {
@@ -60,9 +37,9 @@ function mysql_load()
   var connection = mysql.createConnection({
     host    :'localhost',
     port : 3306,
-    user : 'contact',
+    user : 'mysql',
     password : '1111',
-    database:'contact',
+    database:'4linux',
     insecureAuth : true
   });
   connection.connect(function(err)
@@ -77,52 +54,7 @@ function mysql_load()
   return connection;
 }
 
-function insert_op(request, response)
-{
-  var body = '';
-  const chunks = [];
-  request.on('data', chunk => chunks.push(chunk));
-  request.on('end', () =>
-  {
-    data = qs.parse(Buffer.concat(chunks).toString());
-    console.log('Data : ', data);
-    console.log('name : ', data.name);
-
-    var connection = mysql_load();
-    if(data.name == "" || data.phone == ""){}
-    else{
-      data.email = (data.email == "")? "-" : data.email;
-      data.group = (data.group == "")? "-" : data.group;
-      data.birth = (data.birth == "")? "-" : data.birth;
-    connection.query('INSERT INTO contacts(Name, Phone, Email, Members, Birth) VALUES(?, ?, ?, ?, ?)',
-     [data.name, data.phone, data.email, data.group, data.birth]
-     //, data.email, data.group, data.birth]
-     , function(err, results){
-        if(err)
-        console.log(err);
-    });
-    }
-
-    connection.query('SELECT * FROM contacts', function(err, results)
-    {
-      if(err)
-        console.log(err);
-      else{
-      console.log(results);
-      fs.readFile('html/list.html', 'utf-8', function(error, datas){
-        if(error)
-          console.log('Readfile error');
-        else{
-          response.send(ejs.render(datas, {contactsList:results}));
-        }
-      });
-      }
-    });
-  });
-
-}
-
-function search_op(request, response)
+function search_op_logs(request, response)
 {
   var body = '';
   const chunks = [];
@@ -133,8 +65,9 @@ function search_op(request, response)
     data = qs.parse(Buffer.concat(chunks).toString());
     console.log('Data : ', data);
     var connection = mysql_load();
-    connection.query('SELECT * FROM contacts where Name=? OR Phone=? OR Email=? OR Members=? OR BIRTH=?', [data.name, data.phone, data.email, data.group, data.birth], function(err, results)
-    {
+    connection.query('SELECT * FROM logs where Date > ? or Date < ? order by Date desc',
+    [data.starttime, data.endtime],
+    function(err, results){
       if(err)
         console.log(err);
       else{
@@ -142,7 +75,7 @@ function search_op(request, response)
         if(error)
           console.log('Readfile error');
         else{
-          response.send(ejs.render(datas, {contactsList:results}));
+          response.send(ejs.render(datas, {logList:results}));
         }
       });
       }
@@ -151,10 +84,68 @@ function search_op(request, response)
 
 }
 
-function default_op(request, response)
+function search_op_wakeuplog(request, response)
+{
+  var body = '';
+  const chunks = [];
+  var _data;
+  request.on('data', chunk => chunks.push(chunk));
+  request.on('end', () =>
+  {
+    data = qs.parse(Buffer.concat(chunks).toString());
+    var connection = mysql_load();
+    connection.query('SELECT * FROM wakeuplog where Alarmon > ? or Alarmon < ? order by Alarmon desc',
+    [data.starttime, data.endtime],
+    function(err, results){
+      if(err)
+        console.log(err);
+      else{
+      fs.readFile('html/wakeuplist.html', 'utf-8', function(error, datas){
+        if(error)
+          console.log('Readfile error');
+        else{
+          response.send(ejs.render(datas, {wakeuplogList:results}));
+        }
+      });
+      }
+    });
+  });
+
+}
+
+function search_op_avg(request, response)
+{
+  var body = '';
+  const chunks = [];
+  var _data;
+  request.on('data', chunk => chunks.push(chunk));
+  request.on('end', () =>
+  {
+    data = qs.parse(Buffer.concat(chunks).toString());
+    var connection = mysql_load();
+    connection.query('SELECT sec_to_time(Alarmoff - Alarmon) as AVG FROM wakeuplog where (Alarmon > ? or Alarmon < ?) and (Temp > ? or Temp < ?) and (Light > ? or Light < ?) and (Alti > ? or Alti < ?) and (Press > ? or Press < ?)',
+    [data.starttime, data.endtime, data.starttemp, data.endtemp, data.startlight, data.endlight, data.startalti, data.endalti, data.startpress, data.endpress],
+    function(err, results){
+      if(err)
+        console.log(err);
+      else{
+      fs.readFile('html/wakeupavg.html', 'utf-8', function(error, datas){
+        if(error)
+          console.log('Readfile error');
+        else{
+          response.send(ejs.render(datas, {wakeuplogList:results}));
+        }
+      });
+      }
+    });
+  });
+
+}
+
+function default_op_logs(request, response)
 {
   var connection = mysql_load();
-  connection.query('SELECT * FROM contacts', function(err, results)
+  connection.query('SELECT * FROM logs', function(err, results)
   {
     if(err)
       console.log(err);
@@ -170,69 +161,21 @@ function default_op(request, response)
   });
 }
 
-function delete_op(request, response)
+function default_op_wakeuplog(request, response)
 {
-  var body = '';
-  const chunks = [];
-  request.on('data', chunk => chunks.push(chunk));
-  request.on('end', () =>
+  var connection = mysql_load();
+  connection.query('SELECT * FROM wakeuplog', function(err, results)
   {
-    data = qs.parse(Buffer.concat(chunks).toString());
-    console.log('Data : ', data);
-    var connection = mysql_load();
-    connection.query('DELETE FROM contacts WHERE Id=?', [data.id] ,function(err, rows, fields)
-    {
-      if(err)
+    if(err)
       console.log(err);
-    });
-
-    connection.query('SELECT * FROM contacts', function(err, results)
-    {
-      if(err)
-        console.log(err);
+    else{
+    fs.readFile('html/list.html', 'utf-8', function(error, datas){
+      if(error)
+        console.log('Readfile error');
       else{
-      fs.readFile('html/list.html', 'utf-8', function(error, datas){
-        if(error)
-          console.log('Readfile error');
-        else{
-          response.send(ejs.render(datas, {contactsList:results}));
-        }
-      });
+        response.send(ejs.render(datas, {contactsList:results}));
       }
     });
-  });
-}
-
-function update_op(request, response)
-{
-  var body = '';
-  const chunks = [];
-  request.on('data', chunk => chunks.push(chunk));
-  request.on('end', () =>
-  {
-    data = qs.parse(Buffer.concat(chunks).toString());
-    console.log('Data : ', data);
-    var connection = mysql_load();
-    console.log('data :', data.id)
-    connection.query('UPDATE contacts SET Name=?, Phone=?, Email=?, Members=?, Birth=? WHERE Id=?', [data.name, data.phone, data.email, data.group, data.birth, data.id] ,function(err, rows, fields)
-    {
-      if(err)
-      console.log(err);
-    });
-
-    connection.query('SELECT * FROM contacts', function(err, results)
-    {
-      if(err)
-        console.log(err);
-      else{
-      fs.readFile('html/list.html', 'utf-8', function(error, datas){
-        if(error)
-          console.log('Readfile error');
-        else{
-          response.send(ejs.render(datas, {contactsList:results}));
-        }
-      });
-      }
-    });
+    }
   });
 }
--- nodejs/html/javascript/post.js
+++ nodejs/html/javascript/post.js
@@ -1,11 +1,8 @@
 function goPost(action)
 {
 
-  var obj1 = document.getElementById('name').value;
-  var obj2 = document.getElementById('phone').value;
-  var obj3 = document.getElementById('email').value;
-  var obj4 = document.getElementById('group').value;
-  var obj5 = document.getElementById('birth').value;
+  var obj1 = document.getElementById('starttime').value;
+  var obj2 = document.getElementById('endtime').value;
 
   var form = document.createElement("form");
   form.setAttribute("charset", "UTF-8");
@@ -13,28 +10,87 @@ function goPost(action)
   form.setAttribute("action", action);
 
   var hiddenField = document.createElement("input");
-  hiddenField.setAttribute("name", "name");
+  hiddenField.setAttribute("name", "starttime");
   hiddenField.setAttribute("value", obj1);
   form.appendChild(hiddenField);
 
   var hiddenField = document.createElement("input");
-  hiddenField.setAttribute("name", "phone");
+  hiddenField.setAttribute("name", "endtime");
+  hiddenField.setAttribute("value", obj2);
+  form.appendChild(hiddenField);
+
+  document.body.appendChild(form);
+
+  form.submit();
+}
+
+function goPostAvg(action)
+{
+
+  var obj1 = document.getElementById('starttime').value;
+  var obj2 = document.getElementById('endtime').value;
+  var obj3 = document.getElementById('starttemp').value;
+  var obj4 = document.getElementById('endtemp').value;
+  var obj5 = document.getElementById('startlight').value;
+  var obj6 = document.getElementById('endlight').value;
+  var obj7 = document.getElementById('startalti').value;
+  var obj8 = document.getElementById('endalti').value;
+  var obj9 = document.getElementById('startpress').value;
+  var obj10 = document.getElementById('endpress').value;
+
+  var form = document.createElement("form");
+  form.setAttribute("charset", "UTF-8");
+  form.setAttribute("method", "Post"); // Get 또는 Post 입력
+  form.setAttribute("action", action);
+
+  var hiddenField = document.createElement("input");
+  hiddenField.setAttribute("name", "starttime");
+  hiddenField.setAttribute("value", obj1);
+  form.appendChild(hiddenField);
+
+  var hiddenField = document.createElement("input");
+  hiddenField.setAttribute("name", "endtime");
   hiddenField.setAttribute("value", obj2);
   form.appendChild(hiddenField);
 
   var hiddenField = document.createElement("input");
-  hiddenField.setAttribute("name", "email");
+  hiddenField.setAttribute("name", "starttemp");
   hiddenField.setAttribute("value", obj3);
   form.appendChild(hiddenField);
 
   var hiddenField = document.createElement("input");
-  hiddenField.setAttribute("name", "group");
+  hiddenField.setAttribute("name", "endtemp");
   hiddenField.setAttribute("value", obj4);
   form.appendChild(hiddenField);
 
   var hiddenField = document.createElement("input");
-  hiddenField.setAttribute("name", "birth");
+  hiddenField.setAttribute("name", "startlight");
   hiddenField.setAttribute("value", obj5);
+  form.appendChild(hiddenField);
+
+  var hiddenField = document.createElement("input");
+  hiddenField.setAttribute("name", "endtlight");
+  hiddenField.setAttribute("value", obj6);
+  form.appendChild(hiddenField);
+
+  var hiddenField = document.createElement("input");
+  hiddenField.setAttribute("name", "startalti");
+  hiddenField.setAttribute("value", obj7);
+  form.appendChild(hiddenField);
+
+  var hiddenField = document.createElement("input");
+  hiddenField.setAttribute("name", "endalti");
+  hiddenField.setAttribute("value", obj8);
+  form.appendChild(hiddenField);
+
+  var hiddenField = document.createElement("input");
+  hiddenField.setAttribute("name", "startpress");
+  hiddenField.setAttribute("value", obj9);
+  form.appendChild(hiddenField);
+
+  var hiddenField = document.createElement("input");
+  hiddenField.setAttribute("name", "endpress");
+  hiddenField.setAttribute("value", obj10);
   form.appendChild(hiddenField);
 
   document.body.appendChild(form);
